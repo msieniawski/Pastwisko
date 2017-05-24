12b3b6d09d956517332a933c7fad7e8dc7b0ed3e
sienia0@gmail.com <sienia0@gmail.com>
2017-05-24 23:11:46 +0200

    Initial commit

---
+++ src/main/java/pl/edu/agh/controller/HomeController.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world!";
+    }
+
+}
