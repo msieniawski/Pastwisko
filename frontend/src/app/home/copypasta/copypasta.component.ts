086ae55fc8c13f227287d263e56c743c1aea108b
tradwan <tomasz.x.radwan@gmail.com>
2017-05-28 05:10:55 +0200

    Add copypasta component, list it in feed

--- frontend/src/app/home/copypasta/copypasta.component.ts
+++ frontend/src/app/home/copypasta/copypasta.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Copypasta} from "../../model/copypasta";
 
 @Component({
   selector: 'app-copypasta',
   templateUrl: './copypasta.component.html',
   styleUrls: ['./copypasta.component.css']
 })
-export class CopypastaComponent implements OnInit {
+export class CopypastaComponent implements OnInit, OnChanges {
+
+  @Input('copypasta') copypasta: Copypasta;
+
+  rating: number;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getRating();
   }
 
+  getRating() {
+    this.rating = this.copypasta.ratings.map(rating => rating.value).reduce((a, b) => a + b) / this.copypasta.ratings.length;
+  }
 }
